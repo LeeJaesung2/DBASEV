89ba973869d9a9d6f1d0505d672eaf2142e362fa
이재성 <jaesung@jaesungs-MacBook-Pro.local>
2023-03-30 22:55:01 +0900

    chore : add files

---
+++ rasberryPi/drone/main_process.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define NUM_THREADS 3
+
+void main_thred_func(){
+    int i = 0;
+    while(i<1000){
+        i++;
+        printf("B");
+    }
+}
+
+// 각각의 스레드가 수행할 함수 정의
+void *thread_func1(void *arg)
+{
+    int i = 0;
+    while(i<1000){
+        i++;
+        printf("0");
+    }
+    return 0;
+}
+
+void *thread_func2(void *arg)
+{
+    int i = 0;
+    while(i<1000){
+        i++;
+        printf("1");
+    }
+    return 0;
+
+
+}
+
+void *thread_func3(void *arg)
+{
+    int i = 0;
+    while(i<1000){
+        i++;
+        printf("4");
+    }
+    return 0;
+
+}
+
+int main()
+{
+    int i;
+    void *status;
+    int thr_id;
+    time_t begin = clock();
+    // 각각의 스레드를 생성
+    pthread_t threads[NUM_THREADS];
+
+    //첫번째 스레드 생성
+    thr_id = pthread_create(&threads[0], NULL, thread_func1, (void *)begin);
+    if(thr_id < 0){
+        perror("failure create thread");
+    }
+
+    //두번째 스레드 생성
+    thr_id = pthread_create(&threads[1], NULL, thread_func2, (void *)begin);
+    if(thr_id < 0){
+        perror("failure create thread");
+    }
+
+    //세번째 스레드 생성
+    thr_id = pthread_create(&threads[2], NULL, thread_func3, (void *)begin);
+    if(thr_id < 0){
+        perror("failure create thread");
+    }
+
+    //스레드 종료전 메인스레드 기능
+    main_thred_func();    
+
+    // 각각의 스레드가 종료될 때까지 대기
+    for (i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], &status);
+    }
+
+    // 모든 스레드 종료시 메인스레드 기능
+    printf("all of threads are dead");
+
+    return 0;
+}
