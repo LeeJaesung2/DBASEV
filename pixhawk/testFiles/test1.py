20c639ba786881f1d4c8bab140942c6dfb6e488c
sun ho <tjsgh5768@gmail.com>
2023-04-09 00:28:08 +0900

    dronekit 코드 중간 저장

--- pixhawk/testFiles/test1.py
+++ pixhawk/testFiles/test1.py
@@ -13,12 +13,15 @@ while not vehicle.mode.name =='GUIDED' and not vehicle.armed and not api.exit:
     print (" 이륙 중비중 ...")
     time.sleep(1)
 
+cmds = None
+
 # 미션 초기화
 def init_commands():
     print("명령어 초기화중 ...")
     cmds = vehicle.commands
     cmds.clear()
     cmds.upload()
+    cmds.wait_ready()
     print("명령어 초기화 완료")
 
 # 이륙 코드
@@ -45,6 +48,64 @@ def arm_and_takeoff(aTargetAltitude):
             print("목표 고도에 도달했습니다!")
             break
         time.sleep(1)
+
+# 미션 시작 세팅
+def start_mission():
+    print("자동주행 시스템 가동중 ...")
+    vehicle.commands.next=0
+    vehicle.mode = VehicleMode("AUTO")
+
+# 미션 추가 코드
+def add_mission(new_cmd):
+    print(f"새로운 명령어 : {new_cmd} 추가중 ...")
+    cmds.add(new_cmd)
+    cmds.upload()
+    print("추가 완료!!")
+
+# 맨 처음으로 미션 추가 코드
+def add_mission_first(new_cmd):
+    pass
+
+# 모든 미션 없애버리고 새로운 미션 추가 코드
+def add_mission_ignore_all_mission(new_cmd):
+    print("모든 명령 삭제중 ...")
+    cmds.clear()
+    print("모든 명령 삭제 완료!!")
+
+    print(f"새로운 명령어 : {new_cmd} 추가중 ...")
+    cmds.add(new_cmd)
+    cmds.upload()
+    print("추가 완료!!")
+
+# wayPoint 만들어서 명령에 넣기
+def way_point(stop_time=0, target_lat=0, target_lon=0, target_alt=0):
+    new_cmd = Command(
+        0, 0, 0,
+        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+        mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
+        stop_time,
+        0, 0, 0,
+        target_lat,
+        target_lon,
+        target_alt,
+    )
+    return new_cmd
+
+# 특정 위치에 hovering 하기
+def hovering_time(stop_time=1.0, target_lat=0, target_lon=0, target_alt=0):
+    new_cmd = Command(
+        0, 0, 0,
+        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+        mavutil.mavlink.MAV_CMD_NAV_LOITER_TIME,
+        stop_time,
+        0, 0, 0,
+        target_lat,
+        target_lon,
+        target_alt,
+    )
+
+    return new_cmd
+
 
 init_commands()
 arm_and_takeoff(5)
---
+++ 픽스호크/공식문서예시/connecting.py
@@ -0,0 +1,2 @@
+import dronekit_sitl
+sitl = dronekit_sitl.start_default()
