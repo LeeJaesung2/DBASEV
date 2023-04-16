1b9a8254ace5550464c76048e5265320d3dd7c1b
sun ho <tjsgh5768@gmail.com>
2023-04-16 20:38:57 +0900

    픽스호크 file deleted

--- 픽스호크/공식문서/1.startCode.py
+++
@@ -1,7 +0,0 @@
-# 기본 시작 코드
-vehicle = connect("/dev/ttyAMA0", wait_ready=True, baud=57600)
-vehicle.mode = VehicleMode("GUIDED")
-vehicle.armed = True
-while not vehicle.mode.name =='GUIDED' and not vehicle.armed and not api.exit:
-    print (" Getting ready to take off ...")
-    time.sleep(1)
--- 픽스호크/공식문서/2.connecting.py
+++
@@ -1,4 +0,0 @@
-from dronekit import connect
-
-# 첫번째 파라미터
-vehicle = connect('127.0.0.1:14550', wait_ready=True)
