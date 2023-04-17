f16a1e6dbb729e3db746373b984943c143a2382f
LeeJaesung2 <dlwotjd9909@naver.com>
2023-04-17 14:21:00 +0900

    feat : embedding struct to python

--- src/include/embedd.h
+++ src/include/embedd.h
@@ -1,3 +1,4 @@
 #include <stdarg.h>
 
 int embedd(const char *src, const char *func, int arg, ...);
+int embedd_msg(const char *src, const char *func, message msg, carDage car_data, droneData drone_data);
--- src/modules/embedd/embedd.cpp
+++ src/modules/embedd/embedd.cpp
@@ -68,3 +68,62 @@ int embedd(const char *src,const char *func, int arg, ...){
     return PyInt_AsLong(pValue);
     
 }
+
+int embedd_msg(const char *src,const char *func, message msg, carDage car_data, droneData drone_data ){
+    PyObject *pName, *pModule, *pFunc;
+    PyObject *pArgs, *pValue;
+    int i;
+
+    Py_Initialize();
+    PyRun_SimpleString("import sys");
+    PyRun_SimpleString("sys.path.append(\"./python_src\")"); //set python src path
+    pName = PyString_FromString(src); //get python src
+
+    /*Error checking of pName*/
+    pModule = PyImport_Import(pName);
+    Py_DECREF(pName);
+
+    if (pModule != NULL) {
+        pFunc = PyObject_GetAttrString(pModule, func);
+
+        if (pFunc && PyCallable_Check(pFunc)) {
+
+            
+
+            /*value setting here*/
+
+            /*get return value here*/
+
+
+            /*value checking*/
+            if (pValue != NULL) {
+                printf("Result of call: %ld\n", PyInt_AsLong(pValue));
+                Py_DECREF(pValue);
+            }
+            /*No return value*/
+            else {
+                Py_DECREF(pFunc);
+                Py_DECREF(pModule);
+                PyErr_Print();
+                fprintf(stderr,"Call failed\n");
+                return 1;
+            }
+            va_end(ap);
+        }
+        else {
+            if (PyErr_Occurred())
+                PyErr_Print();
+            fprintf(stderr, "Cannot find function \"%s\"\n", func);
+        }
+        Py_XDECREF(pFunc);
+        Py_DECREF(pModule);
+    }
+    else {
+        PyErr_Print();
+        fprintf(stderr, "Failed to load \"%s\"\n", src);
+        return 1;
+    }
+    Py_Finalize();
+    return PyInt_AsLong(pValue);
+    
+}
