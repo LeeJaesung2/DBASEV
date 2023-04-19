97f97644f38f4b6fa6f9075626d5ca147c9f4c86
LeeJaesung2 <dlwotjd9909@naver.com>
2023-04-19 16:10:40 +0900

    feat : get return from python as dictionary

--- src/modules/speed_control/speed_control.cpp
+++ src/modules/speed_control/speed_control.cpp
@@ -6,9 +6,9 @@ void * thread_func1(void *arg)
     int item;
     for (int i = 0; i < 15; i++) {
         if (cq->try_pop(item)) { // try to pop item from the concurrent queue
-            //printf("Popped item %d\n", item);
+            printf("Popped item %d\n", item);
         } else {
-            //printf("Concurrent queue is empty\n");
+            printf("Concurrent queue is empty\n");
         }
     }
     const char* src = "speed_control";
@@ -16,7 +16,7 @@ void * thread_func1(void *arg)
     const char* msg = "struct argument test success";
     carData car_data = {1, 55, 87.4};
     droneData drone_data = {5, 8, 60.8, {122.2, 8.33}};
-    //callPython(src, func, 1, 1);   
+    callPython(src, func, 1, 1);   
     callPythonStruct(src, func, msg, car_data, drone_data);
     return 0;
 }
