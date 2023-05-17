ac4920f819ad70df8a019fbc5bc6b0e3a2b01172
sun ho <tjsgh5768@gmail.com>
2023-05-17 20:05:35 +0900

    translate ko to en

--- python_src/drone.py
+++ python_src/drone.py
@@ -15,15 +15,15 @@ class Drone:
         self.velocity = self.vehicle.airspeed
         self.max_speed = 11.11
 
-        # 현재 gps
+        # Current GPS
         self.gps = self.vehicle.location.global_relative_frame
 
-        # 현재 waypoint
+        # Current waypoint
         self.waypoint = 0
         self.road_id = 0
 
-        # 목표 waypoint
-        self.target_waypoint = 1,
+        # Target waypoint
+        self.target_waypoint = 1
         self.target_waypoint_gps = LocationGlobalRelative(*(0.0, 0.0, 3.3))
         
         self.will_go_waypoint = deque()
@@ -34,7 +34,7 @@ class Drone:
 
 
     def update_drone_target(self):
-        # target waypoint gps 와 드론 gps 사이 거리
+        # Distance between target waypoint gps and drone gps
         dist = self.gps.distance_to(self.target_waypoint_gps)
         
         if dist < 1.5:
@@ -42,7 +42,7 @@ class Drone:
         else:
             nxt_target = False
             
-        # 다음 waypoint update
+        # Update next waypoint
         if nxt_target:
             if self.will_go_waypoint:
                 self.ishovering = False
@@ -62,10 +62,10 @@ class Drone:
             self.velocity = 0
             return
         
-        # 같은 도로에 있는 경우
+        # Same road
         if car_data["road_id"] == self.road_id:
 
-            # 거리가 멀어져서 속도 조절이 필요한 상황
+            # Need to adjust velocity as the distance gets larger
             if car_data["waypoint"] <= self.waypoint - self.waypoint_num:
                 ideal_velocity = car_data["velocity"] - (self.waypoint_dist * (self.waypoint - car_data.waypoint - self.waypoint_num))
 
@@ -76,11 +76,11 @@ class Drone:
                 
                 self.velocity = ideal_velocity
 
-            # 거리가 가까워서 최대 속도를 내야하는 경우
+            # Need to reach maximum speed as the distance gets closer
             else:
                 self.velocity = self.max_speed
         
-        # 다른 도로에 있어 최대 속도를 내야하는 경우
+        # Need to reach maximum speed as it is on a different road
         else:
             self.velocity = self.max_speed
             
@@ -93,77 +93,67 @@ class Drone:
 
     
 
-    #드론 pixhawk 연결함수
+    # Connect to Pixhawk
     def connect_to_pixhawk(self):
-        # 연결할 시리얼 포트의 경로를 지정합니다.
-        connection_string = "/dev/ttyAMA0" #USB : '/dev/ttyACM0' 
+        # Specify the path of the serial port to connect
+        connection_string = "/dev/ttyAMA0" # USB: '/dev/ttyACM0' 
 
-        # 연결합니다.
+        # Connect to the vehicle
         vehicle = connect(connection_string, baud=57600, wait_ready=True)
 
-        # 연결된 기체의 정보를 출력합니다.
+        # Print information about the connected vehicle
         print('Connected to vehicle on: {}'.format(connection_string))
         print('Vehicle mode: {}'.format(vehicle.mode.name))
 
         self.vehicle = vehicle
 
-    # 이륙 함수
+    # Takeoff function
     def arm_and_takeoff_to_pixhawk(self, aTargetAltitude):
-        print("드론 이륙준비...")
-        # 드론 arm
+        print("Drone takeoff preparation...")
+        # Arm the drone
         while not self.vehicle.is_armable:
-            print("드론 arm 가능 대기 중...")
+            print("Waiting for drone to become armable...")
             time.sleep(1)
 
-        print("드론 arm")
+        print("Arming the drone")
         self.vehicle.mode = VehicleMode("GUIDED")
         self.vehicle.armed = True
 
-        # 이륙 고도 도달 대기
+        # Wait for the drone to be armed
         while not self.vehicle.armed:
-            print("드론 arming 중...")
+            print("Waiting for the drone to become armed...")
             time.sleep(1)
 
-        print("드론 이륙")
+        print("Taking off")
         self.vehicle.simple_takeoff(aTargetAltitude)
 
         while True:
-            print("현재 고도: ", self.vehicle.location.global_relative_frame.alt)
+            print("Current altitude: ", self.vehicle.location.global_relative_frame.alt)
             if self.vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
-                print("목표 고도 도달")
+                print("Target altitude reached")
                 break
             time.sleep(1)
 
 
-    # 드론에게 waypoint 리스트를 전달하여 추가하는 함수
+    # Add waypoints to the drone
     def add_waypoints_to_pixhawk(self, waypoint_list):
-        # LocationGlobalRelative 객체를 이용해 waypoint 추가
+        # Add waypoints using LocationGlobalRelative objects
         for waypoint in waypoint_list:
             waypoint_gps = waypoint[2:]
             wp = LocationGlobalRelative(*waypoint_gps)
             self.vehicle.commands.add(wp)
         self.vehicle.flush()
 
-    # 드론 속도 변화 
+    # Set drone airspeed
     def set_airspeed_to_pixhawk(self, airspeed):
         self.vehicle.airspeed = airspeed
-        # """
-        # 드론의 airspeed 값을 설정합니다.
-        # """
-        # msg = self.vehicle.message_factory.command_long_encode(
-        #     0, 0,                                           # target system, target component
-        #     mavutil.mavlink.MAV_CMD_DO_CHANGE_SPEED,         # command
-        #     0,                                              # confirmation
-        #     1, airspeed, -1, 0, 0, 0)                        # params
-        # self.vehicle.send_mavlink(msg)
-        # self.vehicle.flush()
 
-    # 착륙 함수
+    # Landing function
     def land_to_pixhawk(self):
-        self.vehicle.mode = VehicleMode("LAND") # 드론 착륙
-        while not self.vehicle.mode.name=='LAND':
-            print("Waiting for drone to land...")
+        self.vehicle.mode = VehicleMode("LAND") # Land the drone
+        while not self.vehicle.mode.name == 'LAND':
+            print("Waiting for the drone to land...")
             time.sleep(1)
 
-        self.vehicle.armed = False # 드론 시동 종료
-        self.vehicle.close() # 드론 연결 종료
+        self.vehicle.armed = False # Disarm the drone
+        self.vehicle.close() # Close the connection to the drone
