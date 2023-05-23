f534122e6e2f5bfb52269ed43093c7c4e940a737
LeeJaesung2 <dlwotjd9909@gmail.com>
2023-05-23 21:03:37 +0900

    test : neopixel test

--- gpio.sh
+++ gpio.sh
@@ -11,8 +11,11 @@ cd ../..
 cd lib
 git clone https://github.com/jgarff/rpi_ws281x
 cd rpi_ws281x
+apt-get install scons
+scons
 mkdir build
 cd build
+apt install cmake -y
 cmake -D BUILD_SHARED=OFF -D BUILD_TEST=ON ..
 cmake --build .
 make install
