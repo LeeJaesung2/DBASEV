8fd89c2ab6f615d92efcb81691d6ff8b05f7f98d
LeeJaesung2 <dlwotjd9909@naver.com>
2023-05-24 16:49:20 +0900

    test : neopixel red blue test

--- test/neopixel/neopixel.cpp
+++ test/neopixel/neopixel.cpp
@@ -1,5 +1,3 @@
-
-
 static char VERSION[] = "XX.YY.ZZ";
 
 #include <stdint.h>
@@ -25,22 +23,15 @@ static char VERSION[] = "XX.YY.ZZ";
 #include <ws2811/ws2811.h>
 
 
-
-
 // defaults for cmdline options
 #define TARGET_FREQ             WS2811_TARGET_FREQ
-#define GPIO_PIN                18
+#define GPIO_PIN_18             18
+#define GPIO_PIN_23             23
 #define DMA                     10
-
-#define STRIP_TYPE              WS2811_STRIP_GBR		// WS2812/SK6812RGB integrated chip+leds
-
-
+#define STRIP_TYPE              WS2811_STRIP_GBR     // WS2812/SK6812RGB integrated chip+leds
 #define LED_COUNT               9
 
-
-
 int led_count = LED_COUNT;
-
 int clear_on_exit = 0;
 
 ws2811_t ledstring =
@@ -51,7 +42,7 @@ ws2811_t ledstring =
     {
         [0] =
         {
-            .gpionum = GPIO_PIN,
+            .gpionum = GPIO_PIN_18,
             .invert = 0,
             .count = LED_COUNT,
             .strip_type = STRIP_TYPE,
@@ -59,274 +50,92 @@ ws2811_t ledstring =
         },
         [1] =
         {
-            .gpionum = 0,
+            .gpionum = GPIO_PIN_23,
             .invert = 0,
-            .count = 0,
-            .brightness = 0,
+            .count = LED_COUNT,
+            .strip_type = STRIP_TYPE,
+            .brightness = 255,
         },
     },
 };
 
-ws2811_led_t *matrix;
+ws2811_led_t *matrix1;
+ws2811_led_t *matrix2;
 
 static uint8_t running = 1;
 
-void matrix_render(void)
-{
-    int x, y;
+void matrix_render(void) {
+    int x;
 
-    for (y = 0; y < led_count; y++)
-    {
-        ledstring.channel[0].leds[y] = matrix[y];
-    }
-
-}
-
-
-
-void matrix_clear(void)
-{
-    int y;
-
-    for (y = 0; y < (led_count ); y++)
-    {
+    for (x = 0; x < LED_COUNT; x++) {
         
-        matrix[y] = 0;
+        ledstring.channel[0].leds[x] = matrix1[x]; // Pin 18
+        ledstring.channel[1].leds[x] = matrix2[x]; // Pin 15 
         
     }
 }
 
+void matrix_clear(void) {
+    int x;
 
-
-
-
-
-
-
-
-
-
-void parseargs(int argc, char **argv, ws2811_t *ws2811)
-{
-	int index;
-	int c;
-
-	static struct option longopts[] =
-	{
-		{"help", no_argument, 0, 'h'},
-		{"dma", required_argument, 0, 'd'},
-		{"gpio", required_argument, 0, 'g'},
-		{"invert", no_argument, 0, 'i'},
-		{"clear", no_argument, 0, 'c'},
-		{"strip", required_argument, 0, 's'},
-		{"height", required_argument, 0, 'y'},
-		{"width", required_argument, 0, 'x'},
-		{"version", no_argument, 0, 'v'},
-		{0, 0, 0, 0}
-	};
-
-	while (1)
-	{
-
-		index = 0;
-		c = getopt_long(argc, argv, "cd:g:his:vx:y:", longopts, &index);
-
-		if (c == -1)
-			break;
-
-		switch (c)
-		{
-		case 0:
-			/* handle flag options (array's 3rd field non-0) */
-			break;
-
-		case 'h':
-			fprintf(stderr, "%s version %s\n", argv[0], VERSION);
-			fprintf(stderr, "Usage: %s \n"
-				"-h (--help)    - this information\n"
-				"-s (--strip)   - strip type - rgb, grb, gbr, rgbw\n"
-				"-x (--width)   - matrix width (default 8)\n"
-				"-y (--height)  - matrix height (default 8)\n"
-				"-d (--dma)     - dma channel to use (default 10)\n"
-				"-g (--gpio)    - GPIO to use\n"
-				"                 If omitted, default is 18 (PWM0)\n"
-				"-i (--invert)  - invert pin output (pulse LOW)\n"
-				"-c (--clear)   - clear matrix on exit.\n"
-				"-v (--version) - version information\n"
-				, argv[0]);
-			exit(-1);
-
-		case 'D':
-			break;
-
-		case 'g':
-			if (optarg) {
-				int gpio = atoi(optarg);
-/*
-	PWM0, which can be set to use GPIOs 12, 18, 40, and 52.
-	Only 12 (pin 32) and 18 (pin 12) are available on the B+/2B/3B
-	PWM1 which can be set to use GPIOs 13, 19, 41, 45 and 53.
-	Only 13 is available on the B+/2B/PiZero/3B, on pin 33
-	PCM_DOUT, which can be set to use GPIOs 21 and 31.
-	Only 21 is available on the B+/2B/PiZero/3B, on pin 40.
-	SPI0-MOSI is available on GPIOs 10 and 38.
-	Only GPIO 10 is available on all models.
-
-	The library checks if the specified gpio is available
-	on the specific model (from model B rev 1 till 3B)
-
-*/
-				ws2811->channel[0].gpionum = gpio;
-			}
-			break;
-
-		case 'i':
-			ws2811->channel[0].invert=1;
-			break;
-
-		case 'c':
-			clear_on_exit=1;
-			break;
-
-		case 'd':
-			if (optarg) {
-				int dma = atoi(optarg);
-				if (dma < 14) {
-					ws2811->dmanum = dma;
-				} else {
-					printf ("invalid dma %d\n", dma);
-					exit (-1);
-				}
-			}
-			break;
-
-		case 'y':
-			if (optarg) {
-				led_count = atoi(optarg);
-				if (led_count > 0) {
-					ws2811->channel[0].count = led_count ;
-				} else {
-					printf ("invalid led_count %d\n", led_count);
-					exit (-1);
-				}
-			}
-			break;
-
-
-		case 's':
-			if (optarg) {
-				if (!strncasecmp("rgb", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_RGB;
-				}
-				else if (!strncasecmp("rbg", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_RBG;
-				}
-				else if (!strncasecmp("grb", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_GRB;
-				}
-				else if (!strncasecmp("gbr", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_GBR;
-				}
-				else if (!strncasecmp("brg", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_BRG;
-				}
-				else if (!strncasecmp("bgr", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_BGR;
-				}
-				else if (!strncasecmp("rgbw", optarg, 4)) {
-					ws2811->channel[0].strip_type = SK6812_STRIP_RGBW;
-				}
-				else if (!strncasecmp("grbw", optarg, 4)) {
-					ws2811->channel[0].strip_type = SK6812_STRIP_GRBW;
-				}
-				else {
-					printf ("invalid strip %s\n", optarg);
-					exit (-1);
-				}
-			}
-			break;
-
-		case 'v':
-			fprintf(stderr, "%s version %s\n", argv[0], VERSION);
-			exit(-1);
-
-		case '?':
-			/* getopt_long already reported error? */
-			exit(-1);
-
-		default:
-			exit(-1);
-		}
-	}
-}
-
-void matrix_red(void)
-{
-    int y;
-
-    for (y = 0; y < led_count; y++)
-    {
-        
-        
-        matrix[y ] = 0x00000020;  // Red color
-        
-    }
-}
-
-void matrix_blue(void)
-{
-    int y;
-
-    for (y = 0; y < led_count; y++)
-    {
-        
-        matrix[y] = 0x00200000;  // Blue color
-        
+    for (x = 0; x < LED_COUNT; x++) {
+        matrix1[x] = 0;
     }
 }
 
 
-int main(int argc, char *argv[])
-{
+void matrix_mode1(void){
+    for(int x=0; x<led_count; x++){
+        matrix1[x] = 0x00000020; //Red color
+        matrix2[x] = 0x00200000; //Blue color 
+    }
+}
+
+void matrix_mode2(void){
+    for(int x=0; x<led_count; x++){
+        matrix1[x] = 0x00200000; //Blue color
+        matrix2[x] = 0x00000020; //Red color
+    }
+}
+
+
+int main(int argc, char *argv[]) {
     ws2811_return_t ret;
 
     sprintf(VERSION, "%d.%d.%d", VERSION_MAJOR, VERSION_MINOR, VERSION_MICRO);
 
-    //parseargs(argc, argv, &ledstring);
+    matrix1 = (ws2811_led_t*)malloc(sizeof(ws2811_led_t) * LED_COUNT);
+    matrix2 = (ws2811_led_t*)malloc(sizeof(ws2811_led_t) * LED_COUNT);
 
-    matrix = (ws2811_led_t*)malloc(sizeof(ws2811_led_t) * led_count);
-
-    if ((ret = ws2811_init(&ledstring)) != WS2811_SUCCESS)
-    {
+    if ((ret = ws2811_init(&ledstring)) != WS2811_SUCCESS) {
         fprintf(stderr, "ws2811_init failed: %s\n", ws2811_get_return_t_str(ret));
         return ret;
     }
 
-    while (running)
-    {
-        matrix_red();
+    while (running) {
+
+        matrix_mode1();
         matrix_render();
 
-        if ((ret = ws2811_render(&ledstring)) != WS2811_SUCCESS)
-        {
+        if ((ret = ws2811_render(&ledstring)) != WS2811_SUCCESS) {
             fprintf(stderr, "ws2811_render failed: %s\n", ws2811_get_return_t_str(ret));
             break;
-        
-	}
-	usleep(1000000);
-	matrix_blue();
-	matrix_render();
-	if((ret = ws2811_render(&ledstring)) != WS2811_SUCCESS)
-	{
-		fprintf(stderr, "ws2811_render failed: %s\n", ws2811_get_return_t_str(ret));
-		break;
-	}
-        // 15 frames /sec
+        }
+
+        usleep(1000000);
+
+        matrix_mode2();
+        matrix_render();
+
+        if ((ret = ws2811_render(&ledstring)) != WS2811_SUCCESS) {
+            fprintf(stderr, "ws2811_render failed: %s\n", ws2811_get_return_t_str(ret));
+            break;
+        }
+
         usleep(1000000);
     }
 
-    if (clear_on_exit)
-    {
+    if (clear_on_exit) {
         matrix_clear();
         matrix_render();
         ws2811_render(&ledstring);
