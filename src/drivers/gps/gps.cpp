3d7660545c31a01ebdd5e0d2cd4cbac77389461e
unknown <geun0196@naver.com>
2023-05-24 22:06:50 +0900

    vehicle_main process refac

--- src/drivers/gps/gps.cpp
+++ src/drivers/gps/gps.cpp
@@ -1,9 +1,12 @@
 #include <DBASEV/gps.h>
 
 void *getGPS(void* arg){
+    printf("pre open...");
+
     int fd = serialOpen("/dev/serial0", 9600);
 
     while (1) {
+        printf("connecting...");
         if (serialDataAvail(fd)) {
             char temp = (char)serialGetchar(fd);
 
