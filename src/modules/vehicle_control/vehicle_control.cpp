1d77c880a0b3d4f28ece5e6df3b978d071483de6
unknown <geun0196@naver.com>
2023-05-24 23:32:18 +0900

    gps refac

--- src/modules/vehicle_control/vehicle_control.cpp
+++ src/modules/vehicle_control/vehicle_control.cpp
@@ -16,11 +16,11 @@ void* vehicle_control(void* arg)
     int pre_waypoint = 0, now_waypoint;
 
     while (1) {  
-        //cout << "arg : " << arg << endl;
+        cout << "arg : " << arg << endl;
 
         char* gps = static_cast<char*>(arg); 
 
-        //cout << "gps : " << gps << endl;
+        cout << "gps : " << gps << endl;
 
         // if (!isValidGPSData(gps)) {
         //     pre_gps = "";
