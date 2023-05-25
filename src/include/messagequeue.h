7b2e3f9d2ed240ce72b98fbc9ae218c8556c7d83
LeeJaesung2 <dlwotjd9909@naver.com>
2023-05-25 12:59:12 +0900

    feat : add C++ messagequeue receiver

--- src/include/messagequeue.h
+++ src/include/messagequeue.h
@@ -1,3 +1,6 @@
+#ifndef __MESSAGEQUEUE_H__
+#define __MESSAGEQUEUE_H__
+
 #include "visibility.h"
 #include <sys/ipc.h>
 #include <sys/msg.h>
@@ -10,3 +13,6 @@ struct MsgBuf {
 
 int mq_init(key_t key);
 void push(int key_id,msqid_ds buf, MsgBuf msg);
+MsgBuf pop(int key_id, msqid_ds buf);
+
+#endif
