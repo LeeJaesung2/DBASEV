982a4b8e28e648fc29a676d5abe507f7061ae87b
unknown <geun0196@naver.com>
2023-05-29 21:17:49 +0900

    test : communication test

--- src/modules/vehicle_control/vehicle_control.cpp
+++ src/modules/vehicle_control/vehicle_control.cpp
@@ -78,11 +78,11 @@ void* vehicle_control(void* arg)
         //cout << "vehicle cmd : " << sending_communication.c_str() << endl;
         strcpy(msg.buf, sending_communication.c_str());
         push(key_id2,buf2, msg);
-        if(road_id<2){
+        if(road_id<3){
             road_id++;
         }
         else{
-            road_id = 0;
+            road_id = 1;
         }
     }
 }
