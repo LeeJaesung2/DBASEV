c0c5201805b0a9034237d0c544ca59876e293279
unknown <geun0196@naver.com>
2023-05-29 21:47:37 +0900

    test : communication test

--- src/modules/communication/communication.cpp
+++ src/modules/communication/communication.cpp
@@ -39,7 +39,7 @@ void * sender(void *arg)
         }
         comp = cmd.sq;
         
-        message = "fucking cmd.buf";
+        message = cmd.buf;
 		
 		for (int i = 0; i < message.length(); i += max_chunk_size) {
 				int chunk_size = std::min(max_chunk_size, static_cast<int>(message.length() - i));
