36596e65e7bd52c509b20db912a857bf2180a6fd
unknown <geun0196@naver.com>
2023-05-29 22:11:06 +0900

    test : communication test

--- src/modules/communication/communication.cpp
+++ src/modules/communication/communication.cpp
@@ -38,7 +38,8 @@ void * sender(void *arg)
             cout << "sender : " << cmd.buf  << "msg count : " << cmd.sq << endl;
         }
         comp = cmd.sq;
-        message = string(cmd.buf);
+        message = string(cmd->buf);
+
 		for (int i = 0; i < message.length(); i += max_chunk_size) {
 				int chunk_size = std::min(max_chunk_size, static_cast<int>(message.length() - i));
 				temp = message.substr(i, chunk_size);
