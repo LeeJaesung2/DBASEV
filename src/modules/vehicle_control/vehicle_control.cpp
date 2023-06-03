b096fec1c64c9d34aef4ccc92785a8e1a12a75bb
unknown <geun0196@naver.com>
2023-06-04 00:18:27 +0900

    temp3

--- src/modules/vehicle_control/vehicle_control.cpp
+++ src/modules/vehicle_control/vehicle_control.cpp
@@ -64,7 +64,8 @@ void* vehicle_control(void* arg)
         
         if (remain_waypoints < 2) {
             int pre_road_id = road_id;
-            //road_id = findNextRoadId(road_id, current_latitude, current_longitude, graph);
+            cout << "pre_road_id: " << pre_road_id << endl;
+            road_id = findNextRoadId(road_id, current_latitude, current_longitude, graph);
 
             if(pre_road_id != road_id){
                 pre_waypoint = 0;
