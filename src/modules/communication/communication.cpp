1d34a30589dcb26cb73c4572f636f318c71139c9
unknown <geun0196@naver.com>
2023-06-10 17:52:52 +0900

    data rewrite

--- src/modules/communication/communication.cpp
+++ src/modules/communication/communication.cpp
@@ -193,8 +193,8 @@ void * sender(void *arg)
 
 				// Send the message over the serial port
 				ssize_t bytesWritten = write(serial_port, buf, len);
-				sleep(1);
-                
+				usleep(1000);
+
 				if (bytesWritten < 0 || bytesWritten != len) {
 					std::cerr << "Error sending message." << std::endl;
 					close(serial_port);
